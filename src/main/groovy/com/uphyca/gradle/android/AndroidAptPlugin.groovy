1bd95028554c711a5e34b6f6d25b72470b58d0dd
Sosuke Masui <esmasui@gmail.com>
2013-12-09 14:13:27 +0900

    Prepare version 0.9.1

--- src/main/groovy/com/uphyca/gradle/android/AndroidAptPlugin.groovy
+++ src/main/groovy/com/uphyca/gradle/android/AndroidAptPlugin.groovy
@@ -2,7 +2,6 @@
 package com.uphyca.gradle.android
 
 import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.BasePlugin
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -10,11 +9,14 @@ import org.gradle.api.tasks.compile.JavaCompile
 
 class AndroidAptPlugin implements Plugin<Project> {
 
+    private static final String GENERATED_SOURCE_DIR = "source/generated"
+    private static final String INSTRUMENT_TEST_TASK_NAME = 'instrumentTest'
+    private static final String INSTRUMENT_TEST_SOURCE_SET_NAME = 'instrumentTest'
+
     @Override
     void apply(Project project) {
         def hasAppPlugin = project.plugins.hasPlugin AppPlugin
         def hasLibraryPlugin = project.plugins.hasPlugin LibraryPlugin
-        def log = project.logger
 
         // Ensure the Android plugin has been added in app or library form, but not both.
         if (!hasAppPlugin && !hasLibraryPlugin) {
@@ -25,41 +27,52 @@ class AndroidAptPlugin implements Plugin<Project> {
         }
 
         def variants = hasAppPlugin ?
-                project.android.applicationVariants :
-                project.android.libraryVariants
+                project.android.applicationVariants : project.android.libraryVariants
 
         def aptConfiguration = project.configurations.create('apt')
         aptConfiguration.extendsFrom project.configurations.getByName('compile')
 
+        def testAptConfiguration = project.configurations.create(INSTRUMENT_TEST_TASK_NAME + 'Apt')
+        testAptConfiguration.extendsFrom project.configurations.getByName(INSTRUMENT_TEST_TASK_NAME + 'Compile')
+
         project.afterEvaluate {
             variants.all { variant ->
-
-                def aptOutputDir = project.file(new File(project.buildDir, "source/generated"))
-                def aptOutput = new File(aptOutputDir, variant.dirName)
-                def sourceSet = new File(variant.dirName).getName()
-
-                log.debug("----------------------------------------")
-                log.debug("apt output dir: $aptOutputDir")
-                log.debug("apt output: $aptOutput")
-                log.debug("source set: $sourceSet")
-                log.debug("----------------------------------------")
-
-                project.android.sourceSets[sourceSet].java.srcDirs += aptOutput.getPath()
-
-                JavaCompile javaCompile = variant.javaCompile
-
-                javaCompile.options.compilerArgs += [
-                        '-processorpath', project.configurations.apt.getAsPath(),
-                        '-s', aptOutput
-                ]
-                javaCompile.source = variant.javaCompile.source.filter { p ->
-                    return !p.getPath().startsWith(aptOutputDir.getPath())
-                }
-
-                javaCompile.doFirst {
-                    aptOutput.mkdirs()
-                }
+                def configuraton = 'apt'
+                applyApt(project, variant, new File(variant.dirName).getName(), configuraton, project.configurations[configuraton].asPath)
             }
+            variants.testVariant.findAll { it }.each { variant ->
+                def configuration = "${INSTRUMENT_TEST_TASK_NAME}Apt"
+                applyApt(project, variant, INSTRUMENT_TEST_SOURCE_SET_NAME, configuration, project.configurations["apt"].asPath + File.pathSeparator + project.configurations[configuration].asPath)
+            }
+        }
+    }
+
+    def applyApt(project, variant, sourceSet, configuration, processorPath) {
+        def log = project.logger
+        def aptOutputDir = project.file(new File(project.buildDir, GENERATED_SOURCE_DIR))
+        def aptOutput = new File(aptOutputDir, variant.dirName)
+
+        log.debug("----------------------------------------")
+        log.debug("Variant: $variant.name")
+        log.debug("Output directory: $aptOutput")
+        log.debug("Source set: $sourceSet")
+        log.debug("----------------------------------------")
+
+        project.android.sourceSets[sourceSet].java.srcDirs += aptOutput.getPath()
+
+        JavaCompile javaCompile = variant.javaCompile
+
+        javaCompile.options.compilerArgs += [
+                '-processorpath', processorPath,
+                '-s', aptOutput
+        ]
+
+        javaCompile.source = javaCompile.source.filter { p ->
+            return !p.getPath().startsWith(aptOutputDir.path)
+        }
+
+        javaCompile.doFirst {
+            aptOutput.mkdirs()
         }
     }
 }
